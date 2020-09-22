c85e41cbcc2165a9b8b630899d0e836219232ff2
jon deviney <developerdeviney1@gmail.com>
2020-09-22 16:25:38 -0600

    v.1

---
+++ main.js
@@ -0,0 +1,40 @@
+const userUrl = 'https://api.github.com/users/developerdeviney'
+
+const targetEl = document.querySelector('#main-container')
+
+const orgSection = document.querySelector('#org-data')
+
+const theBasics = document.querySelector('#basics')
+
+const theStory = document.querySelector('#story')
+fetch(userUrl)
+    .then(response => response.json())
+    .then(function (data) {
+        console.log(data)
+        const headerEl = document.createElement('p')
+        headerEl.innerText = data.login
+        targetEl.appendChild(headerEl)
+
+        const nameEl = document.createElement('p')
+        nameEl.innerText = data.name
+        targetEl.appendChild(nameEl)
+        
+        const githubEl = document.createElement('p')
+        githubEl.innerText = data.html_url
+        orgSection.appendChild(githubEl)
+
+        const company = document.createElement('p')
+        company.innerText = data.company
+        orgSection.appendChild(company)  
+        
+        const websiteUrl = document.createElement('p')
+        websiteUrl.innerText = data.blog
+        orgSection.appendChild(websiteUrl) 
+
+        const trueStoray = document.createElement("p")
+        trueStoray.innerText = data.bio 
+        theStory.appendChild(trueStoray)
+
+        
+
+    })
